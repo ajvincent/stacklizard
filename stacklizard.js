4e8d00d97ce5c790a719d31225c966bf7b13588d
Alexander J. Vincent <ajvincent@gmail.com>
2020-05-30 14:36:29 -0700

    StackLizard.ancestorsJS()

--- stacklizard.js
+++ stacklizard.js
@@ -2,10 +2,15 @@
 const espree = require("espree");
 const fs = require("fs").promises;
 const path = require("path");
+const acornWalk = require("acorn-walk");
 
 const sourceOptions = {
   loc: true,
   ecmaVersion: 2020,
+};
+
+const SYMBOLS = {
+  foundAncestors: Symbol("Found ancestors"),
 };
 
 function StackLizard(rootDir, options = {}) {
@@ -22,7 +27,32 @@ StackLizard.prototype = {
     const ast = espree.parse(source, sourceOptions);
     this.sources.set(pathToFile, ast);
     return ast;
-  }
+  },
+
+  ancestorsJS: function(pathToFile, lineNumber, functionIndex = 1) {
+    const ast = this.sources.get(pathToFile);
+    let found = null, hits = 0;
+
+    try {
+      acornWalk.ancestor(ast, {
+        FunctionExpression(_, ancestors) {
+          if ((_.loc.start.line != lineNumber))
+            return;
+          hits++;
+          if (hits == functionIndex) {
+            found = ancestors;
+            throw SYMBOLS.foundAncestors;
+          }
+        }
+      });
+    }
+    catch (ex) {
+      if (ex !== SYMBOLS.foundAncestors)
+        throw ex;
+    }
+
+    return found;
+  },
 };
 
 module.exports = StackLizard;
--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const StackLizard = require("./stacklizard.js");
 
 (async function() {
   const lizard = new StackLizard("fixtures/single-file");
-  let ast = await lizard.parseJSFile("fixture.js");
-  console.log(JSON.stringify(ast, null, 2));
+  await lizard.parseJSFile("fixture.js");
+  const ancestors = lizard.ancestorsJS("fixture.js", 19);
+  console.log(ancestors);
 })();
