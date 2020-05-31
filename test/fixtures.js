dd49de856458d173cd2594195cd930098ebc462e
Alexander J. Vincent <ajvincent@gmail.com>
2020-05-31 10:57:00 -0700

    Fixture tests should require configuration.

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,28 +1,43 @@
 "use strict";
-
 const StackLizard = require("../stacklizard.js");
 const assert = require("assert");
+const fs = require("fs").promises;
+const path = require("path");
 
 async function fixtureTest(fixture) {
   const root = "fixtures/" + fixture;
   const lizard = new StackLizard(root);
-  const pathToFile = "fixture.js";
-  await lizard.parseJSFile(pathToFile);
-  lizard.populateMaps(pathToFile);
 
-  const stacks = lizard.getStacksOfFunction(pathToFile, 26);
-  const analysis = lizard.serializeAnalysis(stacks);
+  let json = {};
+  {
+    const jsonSrc = await fs.readFile(
+      path.join(process.cwd(), root, "test-config.json"),
+      { encoding: "utf-8"}
+    );
+    json = JSON.parse(jsonSrc);
+  }
 
-  const fs = require("fs").promises;
-  const path = require("path");
+  if (Array.isArray(json.scripts)) {
+    await Promise.all(json.scripts.map(async path => lizard.parseJSFile(path)));
+    json.scripts.forEach(path => lizard.populateMaps(path));
+  }
+
+  const stackList = json.markAsync.map(params => lizard.getStacksOfFunction(
+    params.path,
+    params.line,
+    params.functionIndex
+  ));
+  const analysis = lizard.serializeAnalysis.apply(lizard, stackList);
+
   await fs.writeFile(
-    path.join(process.cwd(), root + "/actual-callstack.txt"),
-    analysis
+    path.join(process.cwd(), root, "actual-callstack.txt"),
+    analysis,
+    { encoding: "utf-8"}
   );
 
   const expected = await fs.readFile(
-    path.join(process.cwd(), root + "/expected-callstack.txt"),
-    { encoding: "ascii" }
+    path.join(process.cwd(), root, "expected-callstack.txt"),
+    { encoding: "utf-8"}
   );
 
   assert.equal(analysis, expected);
