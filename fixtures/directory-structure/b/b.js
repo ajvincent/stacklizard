05da534b9d75b7e84b14ed195e3a87bc31700bcb
Alexander J. Vincent <ajvincent@gmail.com>
2020-05-31 22:48:29 -0700

    Fixtures: Move an event listener to an external script.

--- fixtures/directory-structure/b/b.js
+++ fixtures/directory-structure/b/b.js
@@ -1,1 +1,7 @@
 const B = new A();
+
+window.addEventListener("load", function() {
+  document.getElementById("B.d").addEventListener(
+    "click", () => output.value = B.d(), true
+  );
+}, true);
