dbea152f718d12f05c27a4452371c2b487009ff0
Alexander J. Vincent <ajvincent@gmail.com>
2020-06-02 20:03:38 -0700

    HTMLDriver should use EventEmitter.

--- test/HTMLDriver.js
+++ test/HTMLDriver.js
@@ -2,6 +2,7 @@
 const assert = require("assert").strict;
 const fs = require("fs").promises;
 const path = require("path");
+const EventEmitter = require("events");
 const HTMLDriver = require("../lib/html");
 
 it(
@@ -17,37 +18,38 @@ it(
     ];
   
     const actualSequence = [];
+    const events = new EventEmitter;
 
-    const driver = new HTMLDriver({
-      oneventhandler(name, location, attrValue) {
-        actualSequence.push([
-          "eventhandler", location.line, location.column, attrValue.substr(0, 5)
-        ]);
-      },
-  
-      onloadscript(src) {
-        actualSequence.push([
-          "loadscript", 0, 0, src.substr(0, 5)
-        ]);
-      },
-  
-      oninlinescript(location, contents) {
-        actualSequence.push([
-          "inlinescript", location.line, location.column, contents.substr(0, 5)
-        ]);
-      },
-  
-      onloadframe(src) {
-        actualSequence.push([
-          "loadframe", 0, 0, src.substr(0, 5)
-        ]);
-      },
+    events.on("eventhandler", (name, location, attrValue) => {
+      actualSequence.push([
+        "eventhandler", location.line, location.column, attrValue.substr(0, 5)
+      ]);
     });
-  
+
+    events.on("loadscript", (src) => {
+      actualSequence.push([
+        "loadscript", 0, 0, src.substr(0, 5)
+      ]);
+    });
+
+    events.on("inlinescript", (location, contents) => {
+      actualSequence.push([
+        "inlinescript", location.line, location.column, contents.substr(0, 5)
+      ]);
+    });
+
+    events.on("loadframe", (src) => {
+      actualSequence.push([
+        "loadframe", 0, 0, src.substr(0, 5)
+      ]);
+    });
+
+    const driver = new HTMLDriver(events);
+
     const fullPath = path.join(process.cwd(), "fixtures/htmlDriver.html");
     const source = await fs.readFile(fullPath, { encoding: "UTF-8" } );
     driver.parseHTML(source);
-  
+
     assert.deepEqual(actualSequence, expectedSequence);
   }
 );
